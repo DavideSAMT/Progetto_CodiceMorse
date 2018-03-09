64309c9167681a6ebdc2ca8ae379873052d736f9
Patrick Sartori <patrick.sartori@samtrevano.ch>
2018-03-09 13:26:16 +0100

    Codice sensore+pompa

---
+++ Codice/waterLevelSensor/waterLevelSensor.ino
@@ -0,0 +1,17 @@
+
+void setup(){
+  Serial.begin(9600);     // Communication started with 9600 baud
+  pinMode(3, OUTPUT);
+}
+
+
+void loop(){
+  int sensor = analogRead(A1); // Incoming analog signal read and appointed sensor
+  Serial.println(sensor);   //Wrote serial port
+  if(sensor<200){
+    digitalWrite(3, HIGH);
+    delay(5000);
+  }else{
+    digitalWrite(3, LOW);
+  }
+}
